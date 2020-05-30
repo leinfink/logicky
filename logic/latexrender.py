630695619ea7272feca2524a6d023745417b94fb
Henrik Hörmann <henrikhh@zedat.fu-berlin.de>
2020-05-30 22:25:21 +0300

    changed arrows, fixed small bug

--- logic/latexrender.py
+++ logic/latexrender.py
@@ -15,21 +15,20 @@ class LatexRenderTableau():
 
     def render(self, path, show_arrows=True):
         self.negatomics = {}
+        self.atomics = {}
         self.node_count = 0
         self.show_arrows = show_arrows
         text = r"\begin{forest}for tree={align=center, l sep=0, s sep+=4mm}"
         text += self.latex_string_replace(
             self.make_tree_string_forest(self.tree.get_tree_as_nested_list()))
         text += r"\end{forest}"
-        f = open(path+"/template.tex", "r")
+        f = open(path+"template.tex", "r")
         latex_preamble = f.read()
         latex_end = r"\end{document}"
         f = open(path+"output.tex", "w")
         f.write(latex_preamble+text+latex_end)
         f.close()
-        os.system("pdflatex -output-directory=latex "
-                  f"-output-format=pdf {path}/output.tex ")
-        return path+"/output.pdf"
+        return "output"
 
     def make_tree_string_forest(self,
                                 nested_tree,
@@ -44,21 +43,22 @@ class LatexRenderTableau():
                     a.form == CTFLogic.syntax.Form.NEGATION and
                     a.subformulas[0].form == CTFLogic.syntax.Form.ATOMIC):
                 string += r", name=NODE"+str(self.node_count)
-                negatomic = False
+                atomic_or_neg = False
                 if a.form == CTFLogic.syntax.Form.ATOMIC:
-                    negatomic = True
-                    key = a.negation().string
+                    atomic_or_neg = True
                 elif a.form == CTFLogic.syntax.Form.NEGATION:
-                    negatomic = True
-                    key = a.string
-                if negatomic:
+                    atomic_or_neg = True
+                if atomic_or_neg:
                     current_branches = []
                     for i, b in enumerate(self.branches):
                         br = [node[0].formula for node in b]
                         if br[:len(abranch)] == abranch:
                             current_branches.append(i)
                     for i in current_branches:
-                        self.negatomics[(key, i)] = self.node_count
+                        if a.form == CTFLogic.syntax.Form.ATOMIC:
+                            self.atomics[(a.string, i)] = self.node_count
+                        elif a.form == CTFLogic.syntax.Form.NEGATION:
+                            self.negatomics[(a.string, i)] = self.node_count
                 self.node_count += 1
             for i in nested_tree[1:]:
                 if isinstance(i, list):
@@ -68,9 +68,11 @@ class LatexRenderTableau():
             # string += r"}]"
             a = CTFLogic.syntax.Formula(nested_tree[0])
             if a.form == CTFLogic.syntax.Form.NEGATION:
-                formula = a
+                formula = a.subformulas[0]
+                othercheck = self.atomics
             elif a.form == CTFLogic.syntax.Form.ATOMIC:
                 formula = a.negation()
+                othercheck = self.negatomics
             else:
                 # shouldn't happen for valid arguments
                 string += "}]"
@@ -82,16 +84,16 @@ class LatexRenderTableau():
                     current_branch_id = i
                     break
             # basically another version of the validity check here
-            if (formula.string, current_branch_id) in self.negatomics:
+            if (formula.string, current_branch_id) in othercheck:
                 string += r"\\[-0.1em]$\times$"
                 string += "}]"
                 if self.show_arrows:
                     string += r"{\draw[->,dotted] () "
-                    string += r"to[out=south west, in=west] ("
+                    string += r"to[out=west, in=west] ("
                     # get partner element
                     string += r"NODE" + str(
-                        self.negatomics[(formula.string,
-                                         current_branch_id)]) 
+                        othercheck[(formula.string,
+                                    current_branch_id)])
                     string += r");}"
             else:
                 string += r"\\[-0.2em]\scriptsize open}]"
